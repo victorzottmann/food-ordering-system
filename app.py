5c34af3d7580d675b72364d4ff322a0a5d3c0860
Victor Zottmann <zottmann.victor@gmail.com>
2023-12-26 18:30:26 +1100

    add functions

--- app.py
+++ app.py
@@ -1,27 +1,42 @@
-import sys
+import os
 
-print("""
-▀█▀ ▄▀█ █▀ ▀█▀ █▄█   ▀█▀ ▄▀█ █▄▄ █░░ █▀▀
-░█░ █▀█ ▄█ ░█░ ░█░   ░█░ █▀█ █▄█ █▄▄ ██▄
-""")
-
-print("1. Register restaurant")
-print("2. List restaurant")
-print("3. Activate restaurant")
-print("4. Exit\n")
-
-while True:
-  option = int(input("Choose an option between 1 to 4: "))
+def display_menu():
+  # os.system("cls") # on windows
+  os.system("clear") # on mac / linux
   
-  if option == 1:
-    print("Register restaurant")
-    break
-  if option == 2:
-    print("List restaurant")
-    break
-  if option == 3:
-    print("Activate restaurant")
-    break
-  if option == 4:
-    print("Exit")
-    break
+  print("""
+  ▀█▀ ▄▀█ █▀ ▀█▀ █▄█   ▀█▀ ▄▀█ █▄▄ █░░ █▀▀
+  ░█░ █▀█ ▄█ ░█░ ░█░   ░█░ █▀█ █▄█ █▄▄ ██▄
+  """)
+  
+  print("1. Register restaurant")
+  print("2. List restaurant")
+  print("3. Activate restaurant")
+  print("4. Exit\n")
+  
+def exit_program():
+  print("Bye!")
+  
+def select_option():
+  while True:
+    option = int(input("Choose an option between 1 to 4: "))
+    match option:
+      case 1:
+        # register_restaurant()
+        break
+      case 2:
+        # list_restaurant()
+        break
+      case 3:
+        # activate_restaurant()
+        break
+      case 4:
+        exit_program()
+        break
+  
+def main():
+  display_menu()
+  select_option()
+  
+if __name__ == "__main__":
+  main()
