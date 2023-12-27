d7669000d84812e109c914f6eaffa6bb46e21b9d
Victor Zottmann <zottmann.victor@gmail.com>
2023-12-27 23:07:42 +1100

    add list of restaurants as dictionary

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 import os
 
-restaurants = ["Pizza", "Sushi", "Bacon"]
+restaurants = [
+    {
+        "name": "The Tavern", 
+        "category": "Japanese", 
+        "active": False
+    },
+    {
+        "name": "Supreme Pizza",
+        "category": "Italian",
+        "active": True
+    },
+    {
+        "name": "The Canteen",
+        "category": "Mediterranean",
+        "active": True
+    }
+]
 
 def display_menu():
     # os.system("cls") # on windows
@@ -45,8 +61,11 @@ def register_restaurant():
   
 def list_restaurants():
     show_option_title("All restaurants registered:\n")
-    for item in restaurants:
-        print(f"- {item}", sep="\n") 
+    for restaurant in restaurants:
+        name = restaurant["name"]
+        category = restaurant["category"]
+        active = restaurant["active"]
+        print(f"- {name} | {category} | {active}", sep="\n") 
     return_to_menu()       
 
 def show_option_title(title):
