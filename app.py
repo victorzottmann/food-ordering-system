00469d420d1562d9f2c99791de219ca9f8d0b0b3
Victor Zottmann <zottmann.victor@gmail.com>
2024-04-27 16:02:05 +1000

    add method to list all restaurants

--- app.py
+++ app.py
@@ -3,7 +3,4 @@ from models.restaurant import Restaurant
 r_pizza = Restaurant("Pizza", "Italian", True)
 r_sushi = Restaurant("Sushi", "Japanese")
 
-restaurants = [r_pizza, r_sushi]
-
-print(r_pizza)
-print(r_sushi)
+Restaurant.list_restaurants()
--- models/restaurant.py
+++ models/restaurant.py
@@ -1,8 +1,15 @@
 class Restaurant:
-  def __init__(self, name, category, active = False):
+  restaurants = []
+  
+  def __init__(self, name, category, active = False) -> None:
     self.name = name
     self.category = category
     self.active = active
+    Restaurant.restaurants.append(self)
   
-  def __str__(self):
+  def __str__(self) -> str:
     return f"Name: {self.name}, Category: {self.category}, Active: {self.active}"
+  
+  def list_restaurants() -> None:
+    for r in Restaurant.restaurants:
+      print(f"Name: {r.name}, Category: {r.category}, Active: {r.active}")
