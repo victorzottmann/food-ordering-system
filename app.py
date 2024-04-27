7855c0116c92f340372aa53a725195d9f50f6064
Victor Zottmann <zottmann.victor@gmail.com>
2024-04-27 15:41:21 +1000

    add simple Restaurant class

---
+++ app.py
@@ -0,0 +1,5 @@
+from models.restaurant import Restaurant
+
+r_pizza = Restaurant("Pizza", "Italian", True)
+
+print(vars(r_pizza))
---
+++ models/restaurant.py
@@ -0,0 +1,6 @@
+class Restaurant:
+  def __init__(self, name, category, active = False):
+    self.name = name
+    self.category = category
+    self.active = active
+
