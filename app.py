d421fa02f5edc7fe494cefcc4e288f5ed19128d9
Victor Zottmann <zottmann.victor@gmail.com>
2024-04-27 15:51:19 +1000

    add to string method to print restaurant details

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from models.restaurant import Restaurant
 
 r_pizza = Restaurant("Pizza", "Italian", True)
+r_sushi = Restaurant("Sushi", "Japanese")
 
-print(vars(r_pizza))
+restaurants = [r_pizza, r_sushi]
+
+print(r_pizza)
+print(r_sushi)
--- models/restaurant.py
+++ models/restaurant.py
@@ -3,4 +3,6 @@ class Restaurant:
     self.name = name
     self.category = category
     self.active = active
-
+  
+  def __str__(self):
+    return f"Name: {self.name}, Category: {self.category}, Active: {self.active}"
