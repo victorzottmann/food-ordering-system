ec8bf06535896ebd059050d9677e84fe24812997
Victor Zottmann <zottmann.victor@gmail.com>
2024-04-28 22:04:37 +1000

    add main method

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from models.restaurant import Restaurant
 
 r_pizza = Restaurant("pizza", "italian")
 r_sushi = Restaurant("sushi", "japanese")
-
 r_sushi.toggle_active()
 
-Restaurant.list_restaurants()
+def main():
+  Restaurant.list_restaurants()
+
+if __name__ == "__main__":
+  main()
