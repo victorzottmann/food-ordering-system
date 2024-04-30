93bf7a5c55f06aa92726173fec4907124e83d37f
Victor Zottmann <zottmann.victor@gmail.com>
2024-04-30 18:25:24 +1000

    add method to get the average rating of a restaurant

--- models/restaurant.py
+++ models/restaurant.py
@@ -17,15 +17,25 @@ class Restaurant:
     self._active = not self._active
     
   def rate(self, client, score):
-    grade = Rating(client, score)
-    self._ratings.append(grade)
+    rating = Rating(client, score)
+    self._ratings.append(rating)
     
   @classmethod
   def list_restaurants(cls) -> None:
-    print(f"{'Name'.ljust(25)} | {'Category'.ljust(25)} | Status")
+    print(f"{'Name'.ljust(25)} | {'Category'.ljust(25)} | {'Rating'.ljust(25)} | Status")
     for r in cls.restaurants:
-      print(f"{r._name.ljust(25)} | {r._category.ljust(25)} | {r._active}")
+      print(f"{r._name.ljust(25)} | {r._category.ljust(25)} | {str(r.get_average_rating).ljust(25)} | {r._active}")
 
   @property
-  def active(self):
+  def active(self) -> str:
     return "Active" if self._active else "Inactive"
+  
+  @property
+  def get_average_rating(self) -> float:
+    if not self._ratings:
+      return 0
+    # This is a list comprehension that outputs all the ratings in self._ratings (list), then sum them up
+    ratings_sum = sum(rating._score for rating in self._ratings) 
+    total_ratings = len(self._ratings)
+    avg = round(ratings_sum / total_ratings, 1)
+    return avg
